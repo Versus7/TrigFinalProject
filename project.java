5d4de5d06dc49eac31829f8469101b435b7a3f4b
Vir <virn.shah@gmail.com>
2022-05-10 11:32:58 -0700

    Formatting issues

--- project.java
+++ project.java
@@ -8,11 +8,10 @@ public class project {
 
             System.out.println("Rolling " + (int)(numSide) + "-sided die " + (int)(numSide) + " times.");
 
-            System.out.println();
             System.out.println("Chance of not rolling any given number: " + Math.pow((numSide-1.0)/numSide,numSide));
             System.out.printf("Value of 1/E: %37.9f", 1.0/Math.E);
             System.out.println();
-
+            System.out.println();
         }
         /*
         */
